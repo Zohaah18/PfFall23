724524c3c5ef2e3697cf3f46c15686662f52626e
Zohaah18 <142868228+Zohaah18@users.noreply.github.com>
2023-10-24 10:45:32 +0500

    Add files via upload

---
+++ lab08/q4.c
@@ -0,0 +1,22 @@
+/**
+*Programmer:Zoha Ahmed
+*Date:24 October 2023
+*Desc:question 2,lab 6
+*/
+#include<stdio.h>
+int main(){
+  char password[100];
+  char storedpassword[] = "Secure123";
+  printf("Enter you password: ");
+  scanf("%s", password);
+  if(strlen(password) < 8){
+  printf("Password must be 8 characters long\n");
+}
+  if(strcmp(password,storedpassword)==0){
+  printf("Login successful");
+} else{ 
+  printf("Login failed");
+}
+return 0;
+}//end main
+  
