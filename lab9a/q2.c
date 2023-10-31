09925885228143a94352d0b6765e7bd882a3de1e
Zohaah18 <142868228+Zohaah18@users.noreply.github.com>
2023-10-31 08:50:22 +0500

    Add files via upload

---
+++ lab9a/q2.c
@@ -0,0 +1,23 @@
+/**
+*Programmer:Zoha Ahmed
+*Date: 31 October 2023
+*Desc: question 1, lab 9a
+*/
+#include <stdio.h>
+void reverse(int *arr, int size){
+  printf("Array elements in reverse order: \n");
+ for(int i= 10-1; i >=0; i--){
+ printf("%d", arr[i]);
+}
+}
+ int main(){
+  int i;
+  int size = 10;
+ int arr[10];
+ for (int i=0; i < 10; i++){ 
+printf("Enter the number of elements in the array:\n ");
+scanf("%d", &arr[i]);
+}
+  reverse(arr,size);
+}
+
