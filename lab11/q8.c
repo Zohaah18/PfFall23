4206804080c4e1c9aaf975e76f414490a3234dec
Zohaah18 <142868228+Zohaah18@users.noreply.github.com>
2023-11-28 22:39:34 +0500

    Add files via upload

---
+++ lab11/q8.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// Function to calculate the volume of a box
+int calculateVolume(int length, int width, int height) {
+    return length * width * height;
+}
+
+// Function to find transportable boxes and print their volumes
+void findTransportableBoxes(int num_boxes, int boxes[][3]) {
+	int i;
+    for (int i = 0; i < num_boxes; i++) {
+        int box_height = boxes[i][2];
+
+        if (box_height < 41) {
+            int volume = calculateVolume(boxes[i][0], boxes[i][1], box_height);
+            printf("%d\n", volume);
+        }
+    }
+}
+
+int main() {
+    // Sample Input
+    int num_boxes = 4;
+    int boxes[4][3] = {
+        {5, 5, 5},
+        {1, 2, 40},
+        {10, 5, 41},
+        {7, 2, 42}
+    };
+
+    // Find and print transportable volumes
+    findTransportableBoxes(num_boxes, boxes);
+
+    return 0;
+}
+
